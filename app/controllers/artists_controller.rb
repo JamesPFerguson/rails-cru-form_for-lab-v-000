425abe196bdece4aac122f460a23a4015c492a68
James Ferguson <ncjamesferguson@gmail.com>
2018-03-18 00:54:07 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -16,6 +16,15 @@ class ArtistsController < ApplicationController
   end
 
   def show
+    
   end
 
+
+  def s_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def a_params
+    params.require(:name, :bio)
+  end
 end
