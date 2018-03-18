8f3934afbcb9fd0c7e397ebd14dc7ffb4d63ee58
James Ferguson <ncjamesferguson@gmail.com>
2018-03-18 00:51:08 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,4 +3,19 @@ class ArtistsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
 end
